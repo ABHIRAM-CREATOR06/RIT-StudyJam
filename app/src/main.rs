e95ecd214da4d51eaf17a854f2b7229dd4cc923a
ABHIRAM-CREATOR06 <abhirampraneeth2005@gmail.com>
2026-01-11 20:38:31 +0530

    init

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    // user number input cheyyum then even or odd check
+    let mut input = String::new();
+    println!("enter a number : ");
+    io::stdin().read_line(&mut input).expect("error");
+    let number : i32 = input.trim().parse().expect("conversion error");
+    if number%2==0{
+        println!("even");
+    }else{
+        println!("odd");
+    }
+
 }
